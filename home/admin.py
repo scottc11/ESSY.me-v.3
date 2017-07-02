b4f868d3b0c290ed5b77263f995b3495f15667cb
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2017-07-02 17:18:09 -0400

    built the serializer file.  Setup the views imports

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
+from django.shortcuts import get_object_or_404
 from django.contrib import admin
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Project
+from .serializers import ProjectSerializer
 
 # Register your models here.
+admin.site.register(Project)
