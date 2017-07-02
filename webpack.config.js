e6aa51d327b57d01d15749f8790ac39246f7a205
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2017-07-02 19:31:36 -0400

    successful implemented webpack. Still need to integrate it into the django project however.

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+let path = require('path');
+let webpack = require('webpack');
+let BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  // the base directry (absolute path) for resolving the entry option
+  context: __dirname,
+
+  // the entry point we created earlier. Note that './' means
+  // your current directory. You don't have to specify the extension now,
+  // because you will specify extensions later in the `resolve` section
+  entry: './assets/js/index',
+
+  output: {
+    // where you want your compiled bundle to be stored
+    path: path.resolve('./assets/bundles/'),
+    // naming convention webpack should use for your files
+    filename: 'bundle.min.js',
+  },
+
+  plugins: [
+    // telss webpack where to store data about your bundles
+    new BundleTracker({filename: './webpack-stats.json'}),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ],
+
+  module: {
+    loaders: [
+      // a regexp that tells webpack to use the following loads on all .js and .jsx files
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          // specify that we will be dealing with React code
+          presets: ['react']
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    // tells webpack where to look for modules
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']
+  }
+}
