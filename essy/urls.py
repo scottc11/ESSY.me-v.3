3178564412f3778ac78feb127c636b613fd49dd9
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2017-09-17 21:31:31 -0400

    bilt out the project page.  Just a basic shell right now, still needs a lot of work.  Built the url scheme for project as well.

--- essy/urls.py
+++ essy/urls.py
@@ -4,14 +4,15 @@ from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 from home.views.views import projectsList, home
 from home.views.blog import blog, post_detail
-from home.views.projects import projects
+from home.views.projects import projects, project
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
+    url(r'^$', home, name='home'),
     url(r'^blog/$', blog, name='blog'),
     url(r'^posts/(?P<post_id>[0-9]+)/$', post_detail, name="post_detail"),
-    url(r'^projects/$', projects, name='projects'),
+    url(r'^projects/$', projects, name="projects"),
+    url(r'^projects/(?P<num>[0-9])/$', project, name="project"),
 
     url(r'^api/projects/', projectsList.as_view())
 ]
--- home/views/projects.py
+++ home/views/projects.py
@@ -5,3 +5,7 @@ from home.models.project import Project
 def projects(request):
     projects = Project.objects.order_by('pub_date')
     return render(request, 'projects/projects.html', {'projects': projects})
+
+def project(request, num):
+    project = get_object_or_404(Project, pk=num)
+    return render(request, 'projects/project.html', {'project': project})
